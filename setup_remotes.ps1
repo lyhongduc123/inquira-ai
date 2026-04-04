b5eae0a27557fde21e4d7140ab8d9fbbf80d3d22
lyhduc <lyhongduc.34@gmail.com>
2026-04-04 17:21:36 +0700

    setup

---
+++ setup_remotes.ps1
@@ -0,0 +1,4 @@
+#!bash
+git remote add origin_backend https://www.github.com/lyhongduc123/backend-exegent.git
+git remote add origin_frontend https://www.github.com/lyhongduc123/frontend-exegent.git
+Write-Output "Success!"
---
+++ setup_remotes.sh
@@ -0,0 +1,3 @@
+git remote add origin_backend https://www.github.com/lyhongduc123/backend-exegent.git
+git remote add origin_frontend https://www.github.com/lyhongduc123/frontend-exegent.git
+echo "Success!"
